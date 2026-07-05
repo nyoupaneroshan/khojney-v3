ad642715bdae209c3588480402b73fb49a579a86
nyoupaneroshan <nyoupaneroshan@gmail.com>
2026-07-05 22:26:05 +0545

    minor change in project

--- scripts/run-dev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Persistent dev server launcher. Runs the dev server in the background
-# detached from any terminal session so it survives shell exits.
-cd /home/z/my-project
-exec bun run dev > /home/z/my-project/dev.log 2>&1
